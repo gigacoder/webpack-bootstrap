2a2ab8a30cbf96032716c4110d2cd8cd3b30505b
Parul Mishra <brunocoder@yahoo.com>
2017-05-06 17:49:15 -0700

    webpack.config.js updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,103 @@
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var purify = require('purifycss-webpack');
+'use strict';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const path = require("path");
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
+
+const isProd = process.env.NODE_ENV === 'production'; //true or false
+const cssDev = [
+    'style-loader',
+    'css-loader?sourceMap',
+    'sass-loader',
+    {
+        loader: 'sass-resources-loader',
+        options: {
+            resources: [
+                './src/resources.scss'
+            ],
+        },
+    }];
+const cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: ['css-loader','sass-loader', {
+        loader: 'sass-resources-loader',
+        options: {
+            resources: [
+                './src/resources.scss'
+            ],
+        },
+    }],
+    publicPath: '/dist'
+})
+const cssConfig = isProd ? cssProd : cssDev;
+
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
     entry: {
-        main: './src/main.js'
+        app: './src/app.js',
+        bootstrap: bootstrapConfig
     },
     output: {
-        path: path.join(__dirname,'./public'),
+        path: path.resolve(__dirname, "dist"),
         filename: '[name].bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
+            {
+                test: /\.scss$/, 
+                use: cssConfig
+            },
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
                 exclude: /node_modules/,
-                query: {
-                    presets: ['es2015'] 
-                }
+                use: 'babel-loader'
             },
             {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("css-loader?minimize!sass-loader")
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                  'file-loader?name=images/[name].[ext]',
+                  'image-webpack-loader?bypassOnDebug'
+                ]
             },
-            {
-                test: /\.css$/,
-                loader: "css-loader"
-            },
-            {
-                test: /\.css$/,
-                loader: "css-loader!autoprefixer-loader"
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: "file-loader?name=img/img-[hash:6].[ext]"
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                loader: "url-loader?limit=5000&name=img/img-[hash:6].[ext]"
-            },
-            {
-                test: /\.html$/,
-                loader: "html-loader"
-            }
+            { test: /\.(woff2?)$/, use: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+            { test: /\.(ttf|eot)$/, use: 'file-loader?name=fonts/[name].[ext]' },
+            // Bootstrap 3
+            { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, use: 'imports-loader?jQuery=jquery' }
         ]
     },
-    plugins : [
-        new ExtractTextPlugin('./css/bootstrap.css')
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        hot: true,
+        open: true,
+        stats: 'errors-only'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            hash: true,
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin({
+            filename: '/css/[name].css',
+            disable: !isProd,
+            allChunks: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        // Make sure this is after ExtractTextPlugin!
+        new PurifyCSSPlugin({
+            // Give paths to parse for rules. These should be absolute!
+            paths: glob.sync(path.join(__dirname, 'src/*.html')),
+            purifyOptions : {
+                minify : true
+            }
+        })
     ]
-};
+}
+
 
